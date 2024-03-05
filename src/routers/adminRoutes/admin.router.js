c69a8d717cfe4011f692d61db8dbe6ee1780973b
Tran Dang Quang <quangtran2002qn@gmail.com>
2024-03-05 16:40:18 +0700

    s

--- src/routers/adminRoutes/admin.router.js
+++ src/routers/adminRoutes/admin.router.js
@@ -4,6 +4,16 @@ const AdmninController = require("../../controllers/adminControllers/admin.contr
 
 router.put("/club/:id", AdmninController.approve_club);
 
-router.put("/sport/:id", AdmninController.approve_sport);
+router.put("/staff/:id", AdmninController.approve_staff);
+
+router.post("/staffs", StaffController.create_staff);
+
+router.get("/staffs/:id", StaffController.get_staff);
+
+router.get("/staffs", StaffController.get_all_staffs);
+
+router.put("/staffs/:id", StaffController.update_staff);
+
+router.delete("/staffs/:id", StaffController.delete_staff);
 
 module.exports = router;
